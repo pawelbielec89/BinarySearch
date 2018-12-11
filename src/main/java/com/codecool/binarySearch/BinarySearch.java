8bcd842d000e42736c037fcec6d58506f5f98042
Paul <bielcu@gmail.com>
2018-12-11 11:55:11 +0100

    implement bubbleSort method to BinarySearch class

--- src/main/java/com/codecool/binarySearch/BinarySearch.java
+++ src/main/java/com/codecool/binarySearch/BinarySearch.java
@@ -2,14 +2,15 @@ package com.codecool.binarySearch;
 
 public class BinarySearch {
     public boolean find(int[] elements, int value) {
+        int[] sortElements = bubbleSort(elements);
         int lowIndex = 0;
         int highIndex = elements.length - 1;
         boolean result = false;
-        while (lowIndex<=highIndex){
+        while (lowIndex <= highIndex) {
             int middleIndex = (lowIndex + highIndex) / 2;
-            if (elements[middleIndex] < value) {
+            if (sortElements[middleIndex] < value) {
                 lowIndex = middleIndex + 1;
-            } else if(elements[middleIndex] > value) {
+            } else if (sortElements[middleIndex] > value) {
                 highIndex = middleIndex - 1;
             } else {
                 result = true;
@@ -17,5 +18,22 @@ public class BinarySearch {
             }
         }
         return result;
+    }
+
+    private int[] bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    //swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+        return arr;
     }
 }
